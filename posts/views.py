67449a22ab21b1c9118d48cbb6b99c0265bdbaec
John Shields <shields101@hotmail.com>
2024-07-22 18:29:53 +0000

    Add Post and Profile search and filter

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count, Case, When, IntegerField
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -27,8 +28,22 @@ class PostList(generics.ListCreateAPIView):
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
     ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'reactions__owner__profile',
+        'owner__profile',
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+
     ordering_fields = [
         'like_count',
         'funny_count',
