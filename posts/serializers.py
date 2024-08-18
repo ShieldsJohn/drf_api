c22c16c8e316e191051139141c047588fcc75585
John Shields <shields101@hotmail.com>
2024-08-18 16:23:56 +0000

    Add social media links to posts serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -59,7 +59,8 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
+            'profile_image', 'social_media_link1',
+            'social_media_link2', 'created_at', 'updated_at',
             'title', 'caption', 'image', 'image_filter',
             'reaction_id', 'like_id', 'like_count', 'funny_count',
              'sad_count', 'cute_count', 'celebrate_count',
