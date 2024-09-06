c6ce059e4def379dcc6f2a188d478ca98f3c02b1
John Shields <shields101@hotmail.com>
2024-09-06 16:49:32 +0000

    Debug false

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -76,7 +76,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-shieldsjohn-drfapi-yiy0xgmnqr3.ws.codeinstitute-ide.net',
